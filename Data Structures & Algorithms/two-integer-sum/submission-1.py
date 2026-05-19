9e2c5400ab363fcde2c6cdf411df899fd55499a1
Juan Salcedo <salcedoacostajuanfelipe@gmail.com>
2026-05-19 00:33:16 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        analyzed_num = {}
+        index = 0
+        for num in nums:
+            difference = target - num
+            if difference in analyzed_num:
+                return [analyzed_num[difference], index]
+            analyzed_num[num] = index
+            index += 1
+            # Time Complexity O(n)
+            # Space Complexity O(n)
